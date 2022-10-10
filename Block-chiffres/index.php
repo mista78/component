3277ff6c31f256b85446fa979b10ba1a3c086a4f
aurelien <bihel.aurelien@gmail.com>
2022-10-10 20:27:35 +0200

    block-chiffres

--- Block-chiffres/index.php
+++ Block-chiffres/index.php
@@ -1,23 +1,65 @@
-<div class="Block-chiffres">
-    <h2 class="heading">vos besoins</h2>
-    <p class="Block-chiffres_para">notre mission est de former et des soutenir les individus et les organisations dqans leurs défis de développement. nous accompagnons 20 000 entreprises, nous menons 12 000 missions sur mesure par an et deployons près de 300 projets internationaux dans 50 pays.</p>
-    <p class="Block-chiffres_para Block-chiffres_para--last">vos équipes sont uniques. vos enjeux sont spécifiques. ensemble, analysons vos attentes, vos objectifs et concevons votre solution de formation sur mesure. adaptée, concrète, innovente. rappidement efficasse.</p>
-    <h3 class="heading heading--small">Lorem ipsum dolor sit amet consectetur.</h3>
-    <div class="stats">
-        <div class="stat">
-            <img src="/images/client.jpg" alt="icone client" class="stat_img">
-            <p class="stat_nbr">20 000</p>
-            <p class="stat_para">Lorem, ipsum dolor.</p>
-        </div>
-        <div class="stat stat--blue">
-            <img src="/images/world.png" alt="icone de la plannette terre" class="stat_img">
-            <p class="stat_nbr">50</p>
-            <p class="stat_para">pays</p>
-        </div>
-        <div class="stat">
-            <img src="/images/flag.png" alt="icon de drapau" class="stat_img">
-            <p class="stat_nbr">300</p>
-            <p class="stat_para">lorem</p>
+<section class="wp-block-cegos-cegos-page-section page-section page-section__container">
+    <div class="Block-chiffres">
+        <h2 class="heading">vos besoins</h2>
+        <p class="Block-chiffres_para">notre mission est de former et des soutenir les individus et les organisations dqans leurs défis de développement. nous accompagnons 20 000 entreprises, nous menons 12 000 missions sur mesure par an et deployons près de 300 projets internationaux dans 50 pays.</p>
+        <p class="Block-chiffres_para Block-chiffres_para--last">vos équipes sont uniques. vos enjeux sont spécifiques. ensemble, analysons vos attentes, vos objectifs et concevons votre solution de formation sur mesure. adaptée, concrète, innovente. rappidement efficasse.</p>
+        <h3 class="heading heading--small">Lorem ipsum dolor sit amet consectetur.</h3>
+        <div class="stats">
+            <div class="stat">
+                <img src="/images/client.jpg" alt="icone client" class="stat_img">
+                <p class="stat_nbr">20 000</p>
+                <p class="stat_para">Lorem, ipsum dolor.</p>
+            </div>
+            <div class="stat stat--blue">
+                <img src="/images/world.png" alt="icone de la plannette terre" class="stat_img">
+                <p class="stat_nbr">50</p>
+                <p class="stat_para">pays</p>
+            </div>
+            <div class="stat">
+                <img src="/images/flag.png" alt="icon de drapau" class="stat_img">
+                <p class="stat_nbr">300</p>
+                <p class="stat_para">lorem</p>
+            </div>
+            <div class="stat stat--blue">
+                <img src="/images/client.jpg" alt="icone client" class="stat_img">
+                <p class="stat_nbr">20 000</p>
+                <p class="stat_para">Lorem, ipsum dolor.</p>
+            </div>
+            <div class="stat">
+                <img src="/images/world.png" alt="icone de la plannette terre" class="stat_img">
+                <p class="stat_nbr">50</p>
+                <p class="stat_para">pays</p>
+            </div>
+            <div class="stat">
+                <img src="/images/flag.png" alt="icon de drapau" class="stat_img">
+                <p class="stat_nbr">300</p>
+                <p class="stat_para">lorem</p>
+            </div>
+            <div class="stat stat--blue">
+                <img src="/images/world.png" alt="icone de la plannette terre" class="stat_img">
+                <p class="stat_nbr">50</p>
+                <p class="stat_para">pays</p>
+            </div>
+            <div class="stat">
+                <img src="/images/client.jpg" alt="icone client" class="stat_img">
+                <p class="stat_nbr">20 000</p>
+                <p class="stat_para">Lorem, ipsum dolor.</p>
+            </div>
+            <div class="stat stat--blue">
+                <img src="/images/world.png" alt="icone de la plannette terre" class="stat_img">
+                <p class="stat_nbr">50</p>
+                <p class="stat_para">pays</p>
+            </div>
+            <div class="stat">
+                <img src="/images/flag.png" alt="icon de drapau" class="stat_img">
+                <p class="stat_nbr">300</p>
+                <p class="stat_para">lorem</p>
+            </div>
+            <div class="stat stat--blue">
+                <img src="/images/world.png" alt="icone de la plannette terre" class="stat_img">
+                <p class="stat_nbr">50</p>
+                <p class="stat_para">pays</p>
+            </div>
         </div>
     </div>
-</div>
+</section>
