a555ae95c5994498e42844abe1e55cebff8bcc06
aurelien <bihel.aurelien@gmail.com>
2022-10-17 23:43:50 +0200

    block-valeur

---
+++ Block-valeur-ajoutee/index.js
@@ -0,0 +1,22 @@
+// const card = document.querySelector(".Block-valeur-ajoutee_card");
+// const card_video = document.querySelector(".Block-valeur-ajoutee_card--video");
+
+// const btn = document.querySelector(".Block-valeur-ajoutee_btn");
+// const btn_video = document.querySelector(".Block-valeur-ajoutee_btn--video");
+
+// function rotate(btn) {
+//     // if (btn === "btn") {
+//         card.style.transform = "rotateY(180deg)";
+//         card.style.zIndex = "-1"; 
+//         card_video.style.transform = "rotateY(180deg) translateX(100% + 30px)";
+//         card_video.style.zIndex = "1";
+//     // } else {
+//     //     card.style.transform = "rotateY(0deg)";
+//     //     card.style.zIndex = "1"; 
+//     //     card_video.style.transform = "rotateY(0deg) translateX(100% + 30px)";
+//     //     card_video.style.zIndex = "-1";
+//     // }
+// }
+
+// btn.addEventListener('click', rotate);
+// btn_video.addEventListener('click', rotate);
--- Block-valeur-ajoutee/index.php
+++ Block-valeur-ajoutee/index.php
@@ -4,26 +4,40 @@
         <p class="Block-valeur-ajoutee_para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit!</p>
         <div class="Block-valeur-ajoutee_cards">
             <div class="Block-valeur-ajoutee_card">
-                <div class="Block-valeur-ajoutee_card_content">
-                    <h3>Lorem, ipsum dolor.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam hic nesciunt, rem at tempora temporibus..</p>
+                <input type="checkbox" id="btn">
+                <div class="Block-valeur-ajoutee_card_one">
+                    <div>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam hic nesciunt, rem at tempora temporibus..</p>
+                    </div>
+                    <label class="Block-valeur-ajoutee_btn" for="btn"></label>
                 </div>
-                <div class="Block-valeur-ajoutee_btn"></div>
-            </div>
-            <div class="Block-valeur-ajoutee_card Block-valeur-ajoutee_card--video">
-                <div class="Block-valeur-ajoutee_card_content Block-valeur-ajoutee_card_content--video">
-                    <h3>Lorem, ipsum.</h3>
-                    <p class="Block-valeur-ajoutee_card--video--para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam explicabo repellat ea quasi reprehenderit nisi dolorum nam harum deserunt illo!</p>
-                    <p class="Block-valeur-ajoutee_card--video--para--weight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eum iusto, quia veniam expedita ab.</p>
+                <div class="Block-valeur-ajoutee_card_one Block-valeur-ajoutee_card_one--two">
+                    <div>
+                        <h3>Lorem, ipsum.</h3>
+                        <p class="Block-valeur-ajoutee_card_one--two--para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam explicabo repellat ea quasi reprehenderit nisi dolorum nam harum deserunt illo!</p>
+                        <p class="Block-valeur-ajoutee_card_one--two--para--weight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eum iusto, quia veniam expedita ab.</p>
+                    </div>
+                    <label class="Block-valeur-ajoutee_btn Block-valeur-ajoutee_btn--two" for="btn"></label>
                 </div>
-                <div class="Block-valeur-ajoutee_btn Block-valeur-ajoutee_btn--video"></div>
             </div>
             <div class="Block-valeur-ajoutee_card">
-                <div class="Block-valeur-ajoutee_card_content">
-                    <h3>Lorem, ipsum dolor.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae odio soluta inventore illo! Assumenda..</p>
+                <input type="checkbox" id="btn">
+                <div class="Block-valeur-ajoutee_card_one">
+                    <div>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam hic nesciunt, rem at tempora temporibus..</p>
+                    </div>
+                    <label class="Block-valeur-ajoutee_btn" for="btn"></label>
                 </div>
-                <div class="Block-valeur-ajoutee_btn"></div>
+                <div class="Block-valeur-ajoutee_card_one Block-valeur-ajoutee_card_one--two">
+                    <div>
+                        <h3>Lorem, ipsum.</h3>
+                        <p class="Block-valeur-ajoutee_card_one--two--para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam explicabo repellat ea quasi reprehenderit nisi dolorum nam harum deserunt illo!</p>
+                        <p class="Block-valeur-ajoutee_card_one--two--para--weight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eum iusto, quia veniam expedita ab.</p>
+                    </div>
+                    <label class="Block-valeur-ajoutee_btn Block-valeur-ajoutee_btn--two" for="btn"></label>
+                </div>
             </div>
         </div>
     </div>
