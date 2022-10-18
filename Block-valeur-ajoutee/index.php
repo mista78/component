ccac4dcf0ed19dbcfe08285fa69daf19e3dafc94
aurelien <bihel.aurelien@gmail.com>
2022-10-18 16:17:59 +0200

    anim-card-FINAL

--- Block-valeur-ajoutee/index.php
+++ Block-valeur-ajoutee/index.php
@@ -34,9 +34,27 @@
                     <div>
                         <h3>Lorem, ipsum.</h3>
                         <p class="Block-valeur-ajoutee_card_one--two--para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam explicabo repellat ea quasi reprehenderit nisi dolorum nam harum deserunt illo!</p>
-                        <p class="Block-valeur-ajoutee_card_one--two--para--weight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eum iusto, quia veniam expedita ab.</p>
+                        <p class="Block-valeur-ajoutee_card_one--two--para--weight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas corrupti cupiditate ad est nam quasi illum placeat porro debitis eum sapiente assumenda eligendi eveniet alias, iste at deleniti recusandae, molestias quos iure ea impedit optio! Nulla facere fugit inventore ea repellat magnam? Quam incidunt qui nulla atque nisi animi delectus.</p>
                     </div>
                     <label class="Block-valeur-ajoutee_btn Block-valeur-ajoutee_btn--two" for="btn-2"></label>
+                </div>
+            </div>
+            <div class="Block-valeur-ajoutee_card">
+                <input type="checkbox" id="btn-3">
+                <div class="Block-valeur-ajoutee_card_one">
+                    <div>
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam hic nesciunt, rem at tempora temporibus..</p>
+                    </div>
+                    <label class="Block-valeur-ajoutee_btn" for="btn-3"></label>
+                </div>
+                <div class="Block-valeur-ajoutee_card_one Block-valeur-ajoutee_card_one--two">
+                    <div>
+                        <h3>Lorem, ipsum.</h3>
+                        <p class="Block-valeur-ajoutee_card_one--two--para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam explicabo repellat ea quasi reprehenderit nisi dolorum nam harum deserunt illo!</p>
+                        <p class="Block-valeur-ajoutee_card_one--two--para--weight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eum iusto, quia veniam expedita ab.</p>
+                    </div>
+                    <label class="Block-valeur-ajoutee_btn Block-valeur-ajoutee_btn--two" for="btn-3"></label>
                 </div>
             </div>
         </div>
